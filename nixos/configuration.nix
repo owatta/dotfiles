1c54c4e58e7429df2d4a4579bd3955c084b46d03
owatta <arsenii.korniec@bk.ru>
2023-11-18 17:04:34 +0500

    Modularizing configuration...

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ inputs, lib, config, pkgs, ... }:
 
 {
   imports =
@@ -27,6 +27,21 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+ # This will add each flake input as a registry
+  # To make nix3 commands consistent with your flake
+  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+  
   # Enable networking
   networking.networkmanager.enable = true;
 
